888055b3e8b3bff4aef49d7a1a3592540097f923
retbleed <hsa.lapaz@gmail.com>
2024-05-22 14:22:35 -0700

    Pet migration fixed

--- database/migrations/2024_04_30_031883_create_pets_table.php
+++ database/migrations/2024_04_30_031883_create_pets_table.php
@@ -4,21 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('pets', function (Blueprint $table) {
+        Schema::create("pets", function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('name');
-            $table->string('image')->nullable();
-            $table->enum('type', ['dog', 'cat', 'bird', 'fish', 'hamster', 'rabbit', 'other'])->default('dog');
-            $table->string('breed');
-            $table->string('age');
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+            $table->string("name");
+            $table
+                ->enum("type", [
+                    "dog",
+                    "cat",
+                    "bird",
+                    "fish",
+                    "hamster",
+                    "rabbit",
+                    "other",
+                ])
+                ->default("dog");
+            $table->string("breed");
+            $table->string("age");
             $table->string("personality");
             $table->timestamps();
         });
@@ -29,6 +37,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pets');
+        Schema::dropIfExists("pets");
     }
 };
